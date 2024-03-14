f30cd5d5e510649b19d9afbd86ab364df600767d
baongochuhu <142041290+baongoc13122004@users.noreply.github.com>
2024-03-14 07:57:56 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        // Tạo một đối tượng MP3
+        MP3 mp3 = new MP3("Nhung loi hua bo quen", "This is a popular song", "12345", 13.12f);
 
-        MP3 mp3;
+        // Tạo một đối tượng Book
+        Book book = new Book("Java Programming", "Learn Java programming language", "67890", 69.69f, "John Doe", 400, "Programming");
+
+        // Hiển thị thông tin của đối tượng MP3
+        System.out.println("Information of MP3:");
         mp3.showInfo();
+        System.out.println();
 
-        Book book;
+        // Hiển thị thông tin của đối tượng Book
+        System.out.println("Information of Book:");
         book.showInfo();
-
     }
 }
