8240bf33df5df652f4679124d5243d4cde6a66c9
baongochuhu <142041290+baongoc13122004@users.noreply.github.com>
2024-03-14 07:41:09 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,23 @@
-public class Item {
-}
+abstract class Item {
+    public String name;
+    private String description;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    protected String id;
+
+    float price;
+    void showInfo(){
+
+    }
+    public Item(String name, String description, String id, float price){
+        this.description=description;
+        this.name = name;
+        this.id=id;
+        this.price=price;
+    }
+
